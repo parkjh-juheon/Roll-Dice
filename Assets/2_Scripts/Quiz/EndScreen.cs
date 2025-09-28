73509d46b556a91c140beb3602bdbff1aa69ab8b
박주현 <vahyeon2501@sonline20.sen.go.kr>
2025-09-28 22:25:36 +0900

    스코어

--- Assets/2_Scripts/Quiz/EndScreen.cs
+++ Assets/2_Scripts/Quiz/EndScreen.cs
@@ -9,6 +9,27 @@ public class EndScreen : MonoBehaviour
     // 최종 점수 출력
     public void ShowFinalScore()
     {
-        finalScoreText.text = $"축하합니다!\n\n당신의 점수는" + $"{scoreKeeper.CalculateScore()}% 입니다.";
+        int score = scoreKeeper.CalculateScore();
+        string message;
+
+        if (score == 100)
+        {
+            message = "전부 맞췄어, 훌륭한데?";
+        }
+        else if (score >= 80)
+        {
+            message = "거의 완벽해, 잘했어!";
+        }
+        else if (score >= 50)
+        {
+            message = "아쉽네? 좀 더 노력해봐!";
+        }
+        else
+        {
+            message = "많이 틀렸네... 다음엔 더 잘할 수 있을거야!";
+        }
+
+        finalScoreText.text = message;
     }
+
 }
